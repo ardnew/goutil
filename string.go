a367408ea14bd9b2cfd049ed0420804de2c62d8f
ardnew <andrew@ardnew.com>
2018-10-16 16:17:47 -0500

    change: added MD5/SHA1/SHA256 hash digest wrappers

--- string.go
+++ string.go
@@ -8,6 +8,10 @@
 package goutil
 
 import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"fmt"
 	"math/rand"
 	"time"
 	"unicode/utf8"
@@ -22,6 +26,18 @@ func init() {
 	alphaRune = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 	alphaNumericRune = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 	rand.Seed(time.Now().UTC().UnixNano())
+}
+
+func MD5(s string) string {
+	return fmt.Sprintf("%x", md5.Sum([]byte(s)))
+}
+
+func SHA1(s string) string {
+	return fmt.Sprintf("%x", sha1.Sum([]byte(s)))
+}
+
+func SHA256(s string) string {
+	return fmt.Sprintf("%x", sha256.Sum256([]byte(s)))
 }
 
 func Reverse(s string) string {
