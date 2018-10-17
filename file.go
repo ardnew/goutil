5a6e07599554dba9dcc13f73d7d8382b15b65e71
ardnew <andrew@ardnew.com>
2018-10-17 08:19:44 -0500

    change: fixed PathExists() to report false for inaccessible paths in addition to non-existent paths

--- file.go
+++ file.go
@@ -11,13 +11,20 @@ import (
 	"os"
 )
 
-// function PathExists() determines if a file or directory exists at the given
-// path. if it exists, a true value and the associated stat struct is returned.
-// otherwise, a false value and a nil stat struct is returned.
+// function PathExists() determines if a file or directory -can be verified- to
+// exist at the given path under the effective permissions of this running
+// process. if it exists, a true value and the associated stat struct is
+// returned. otherwise, a false value and a nil stat struct is returned.
+//
+//  N.B., this function provides no way of distinguishing an existing file with
+//        insufficient permissions and a non-existing file! in both cases, you
+//        will receive a false return because in both cases there does not exist
+//        a file that can be referenced at the given path.
+//
 func PathExists(p string) (bool, os.FileInfo) {
+
 	info, err := os.Stat(p)
-	exists := err == nil || !os.IsNotExist(err)
-	if !exists {
+	if err != nil && (os.IsNotExist(err) || os.IsPermission(err)) {
 		return false, nil
 	}
 	return true, info
